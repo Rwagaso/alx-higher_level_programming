fbe07949767b6c8b40bee5c39c58a2746f9e58b9
Rwagaso <stevenrwa31@gmail.com>
2023-09-04 03:06:44 -0700

    more

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-Rectangle = __import__('0-rectangle').Rectangle
+"""0-rectangle, 
+"""
 
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
+
+class Rectangle:
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+    pass
